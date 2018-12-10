053660c4dc167a4ceb39671e379e5c18ab39b5a6
kaspychala <kaspychala95@gmail.com>
2018-12-10 02:15:17 +0100

    small fix to data representation

--- data.py
+++ data.py
@@ -5,5 +5,5 @@ actions = {u"otworzyć":{"aplikacja":"open -a ",
  		u"zablokować":{"komputer":"/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend"},
  		u"włączyć":{"aplikacja":"open -a "}, u"zamknąć":{"aplikacja":"pkill "}}
 VERB = ["sec","inf"]
-NOT_NOUN = ["prep", "praep", "npraep", "conj", "comp", "burk", "interp", "akc", "pri"]
+NOT_NOUN = ["prep", "praep", "npraep", "conj", "comp", "burk", "interp", "akc", "pri", "qub"]
 UNKNOWN = "<unknown>"
--- test.py
+++ test.py
@@ -17,30 +17,6 @@ plural = False
 reload(sys)
 sys.setdefaultencoding('utf8')
 tt_pl = TreeTagger(path_to_treetagger='/Users/kasper/Development/Python/SmartTerminal/', language='polish')
-tags = tt_pl.tag("Zamknij dla mnie aplikację Spotify")
+tags = tt_pl.tag("Serwus typiarzu otwórz no dla mnie aplikację Spotify, ale no pędem!")
 for tag in tags:
-    if set(tag[1].split(":")) & set(data.VERB):
-        verb = tag[2]
-        verb_added = True
-    elif verb_added and not set(tag[1].split(":")) & set(data.NOT_NOUN):
-        if  "pl" in tag[1]:
-            plural = True
-        noun = tag[2]
-        noun_added = True
-        verb_added = False
-    elif noun_added and not set(tag[1].split(":")) & set(data.NOT_NOUN):
-        arguments.append(tag[0])
-
-if plural:
-    for argument in arguments:
-        command = data.actions[verb][noun] + argument
-        os.system(command)
-
-else:
-    if not len(arguments) == 0: 
-        command = data.actions[verb][noun] + arguments[0]
-    else:
-        command = data.actions[verb][noun]
-    os.system(command)
-
-#print("("+tag[0]+" - "+tag[1]+" - "+tag[2]+")")
+    print("("+tag[0]+" - "+tag[1]+" - "+tag[2]+")")
